65e4dfbc84110dcac0d375262cb083004341eee4
PJtools <pjtools@vip.qq.com>
2017-12-04 17:52:38 +0800

    初始化.

--- dist/gvue.js
+++ dist/gvue.js
@@ -2474,7 +2474,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-  version: '0.0.3',
+  version: '0.0.4',
   locale: _index2.default.use,
   i18n: _index2.default.i18n,
   install: install,
